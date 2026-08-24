90eb07adfded7193c4325786b68e4c9c7ee4f58a
notdevcody <149296239+notdevcody@users.noreply.github.com>
2026-08-24 19:11:51 +0200

    fix: LWJGLUtil debug logger

--- src/main/java/org/lwjgl/LWJGLUtil.java
+++ src/main/java/org/lwjgl/LWJGLUtil.java
@@ -1,16 +1,18 @@
 package org.lwjgl;
 
-import pl.tomgirl.lumen.Lumen;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("unused")
 public class LWJGLUtil {
     public static final boolean DEBUG = Boolean.getBoolean("org.lwjgl.util.Debug");
+    private static final Logger LOG = LogManager.getLogger("LWJGL");
 
     private LWJGLUtil() {}
 
     public static void log(CharSequence msg) {
         if (DEBUG) {
-            Lumen.LOG.debug(msg);
+            LOG.debug(msg);
         }
     }
 }
